96671b1b0350ec1b31aef45234a455c93e59a5db
quyen9x1995 <boy20195@gmail.com>
2019-04-29 14:02:54 +0700

    Config use ejs in app.js and use ejs template

--- app.js
+++ app.js
@@ -2,24 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 
 const { router: adminRoutes } = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-// const hbs = exphbs.create({
-//     helpers: {
-//         gt: function(a, b) {
-//             return (a > b);
-//         }
-//     }
-// });
-
-// app.engine('hbs', hbs.engine);
-app.engine('hbs', exphbs({ defaultLayout: 'main-layout', layoutsDir: path.join(__dirname, 'views', 'layouts'), extname: '.hbs' }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // app.set('views', [path.join(__dirname, 'views')]);
 
